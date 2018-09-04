a3dfee4a6b35c0c1d2006f8eb0cea922a9ae6e92
Tara Fenton <tarafenton@Taras-MBP-3.fios-router.home>
2018-09-04 15:41:52 -0400

    customers move

--- public/js/Customer.js
+++ public/js/Customer.js
@@ -1,15 +1,14 @@
-import Bar from "./Bar.js";
-
 class Customer {
 
-  constructor (i) {
+  constructor (i, bar) {
     this.i = i;
     this.CUSTOMER_AMOUNT = 4;
     this.CUSTOMER_HEIGHT = 80;
     this.CUSTOMER_WIDTH = 40;
     this.CUSTOMER_START_Y = 100;
     // this.customersObj = {};
-    this.bar = new Bar();
+    this.bar = bar;
+    this.customerObj = {};
   }
 
   setup() {
@@ -19,13 +18,35 @@ class Customer {
     $customerDiv.css("top", (this.CUSTOMER_HEIGHT / 2 + this.bar._padding) * this.i + this.CUSTOMER_START_Y +"px");
     $customerDiv.css("left", "30px");
     // customer object
-    var customerObj = {};
+
     // customerObj.id = "data-customer-index" + i;
-    // customerObj.element = $customerDiv;
-    // customerObj.movingForward = true;
+    this.customerObj.element = $customerDiv;
+    this.customerObj.movingForward = true;
     // //customerObj.drinking = false;
     // customerObj.barRow = i;
     // customersObj[i] = customerObj;
+
+  }
+  customerMovingToBartender(current) {
+
+    //console.log(this.customerObj[current]);
+    this._customer.element.animate(
+      { left: "+=420" },
+      10000 * (current + 1), //SLOW cutomers for game
+      //1000 * (current + 1), //fast cutomers for testing
+      function() {
+        //// KILL THE BARTENDER, CUSTOMER AT END OF BAR
+        //killTheBartender();
+        console.log("killTheBartender");
+      }
+    );
+  }
+  get _customer() {
+    return this.customerObj;
+  }
+
+  set _customer(customerObj) {
+    return (this.customerObj = customerObj);
   }
 }
 export default Customer;
--- public/js/MainClass.js
+++ public/js/MainClass.js
@@ -2,7 +2,10 @@ import Bar from "./Bar.js";
 import Bartender from "./Bartender.js";
 import Beer from "./Beer.js";
 import Customer from "./Customer.js";
+import GameOver from "./GameOver.js";
+import GetReady from "./GetReady.js";
 import Level from "./Level.js";
+import Points from "./Points.js";
 
 var $containerDiv = $("body").append("<div id='container'></div>");
 
@@ -12,11 +15,20 @@ bar.setup();
 var $bartenderDiv = $("<div id='bartender'></div>");
 $("#container").append($bartenderDiv);
 
-const bartender = new Bartender();
-bartender.setup();
-
 var $customersDiv = $("<div class='customers'></div>");
 $("#container").append($customersDiv);
+
+
+// const points = new Points();
+// points.setup();
+
+// const getReady = new GetReady();
+// getReady.setup();
+
+// const gameOver = new GameOver();
+// gameOver.setup();
+
+
 
 // var $levelDiv = $("<div id='level'></div>");
 // $("#container").append($levelDiv);
@@ -29,22 +41,103 @@ $("#container").append($customersDiv);
 
 // $levelDiv.append(Level.level);
 
-let customers = [];
+
+const bartender = new Bartender();
+bartender.setup();
+
+const customers = [];
 function makeCustomers() {
   for (let i = 0; i < 4; i++){
-    const customer = new Customer(i);
+    const customer = new Customer(i, bar);
     customer.setup();
     customers.push(customer);
+    setTimeout(customer.customerMovingToBartender(i), 30000); // something random
   }
 }
 makeCustomers();
+
 console.log(customers);
 
 let beers = [];
 let beerCount = 0;
-let beer = new Beer(beerCount);
-beer.setup();
-beers.push(beer);
+let pouring = false;
+let pouringSent = false;
+function makeBeers() {
+    const beer = new Beer(beerCount, bartender);
+    beer.setup();
+}
+
+
+//// GET CUSTOMER COLLISONS - SET INTERVAL
+// interval used to test for when all customers are served
+var customerInterval = setInterval(getCustomers, 500);
+function getCustomers() {
+  // set the total customers to the number of customer objects
+  // let totalCustomers = Object.keys(customers).length;
+  // // will reset each time getCustomer interval is triggered
+  // let countCustomersReturning = 0;
+  // // for each customer
+  // for (var c = 0; c < totalCustomers; c++) {
+  //   //check if they are moving back
+  //   if(!customers[c].movingForward) {
+  //     console.log("totalCustomers",customers[c]);
+  //     //cuz they have to be moving back to door
+  //     countCustomersReturning++;
+  //     // check if they are at the door
+  //     // if (parseInt(customers[c]._customer.element.css('left')) < CUSTOMER_START_Y - CUSTOMER_WIDTH) {
+  //     if (parseInt(customers[c]._customer.element.css('left')) < 100 - 40) {
+  //       customers[c].element.css('display', 'none');
+  //     }
+  //   }
+  // }
+  // if (countCustomersReturning === totalCustomers) {
+  //   checkForBeers = true;
+  // }
+  // // else {
+  // //   countCustomersReturning = 0;
+  // // }
+  // // now that all customers are returning
+  // // check if all glasses are collected
+  // totalBeers = Object.keys(beersObj).length;
+  // countBeersCollected = 0;
+  //
+  // if (checkForBeers) {
+  //   // for each beer
+  //   for (var b = 0; b < Object.keys(beersObj).length; b++) {
+  //     // check if all glasses are collected
+  //     if(beersObj[b].collected) {
+  //       countBeersCollected++
+  //     }
+  //   }
+  //   // ALL BEERS WERE COLLECTED - LEVEL WON!
+  //    if (countBeersCollected === totalBeers && totalBeers > 0) {
+  //   //if (countBeersCollected === totalBeers) {
+  //     levelWon = true;
+  //   } else {
+  //     countBeersCollected = 0;
+  //   }
+  // }
+  /// TO DO : make this a function?
+  // if (levelWon) {
+  //   // ADD POINTS
+  //   // 1000 Points for completing a level
+  //   points += 1000;
+  //   $pointsDiv.text(points);
+  //   // ADD LEVEL
+  //   level++;
+  //   CUSTOMER_AMOUNT = CUSTOMER_AMOUNT * level;
+  //   $levelDiv.text(level);
+  //   //reset value for next level
+  //   levelWon = false;
+  //   // clear the beer and customer intervals
+  //   clearInterval(beerInterval);
+  //   clearInterval(customerInterval);
+  //   // NEXT LEVEL
+  //   nextLevel();
+  // }
+}
+
+
 
 /////////////////////////////////////////// KEY DOWN /////////////////
 $("body").on("keydown", function(evt) {
@@ -55,21 +148,22 @@ $("body").on("keydown", function(evt) {
   var keyPressed = event.which;
   switch (keyPressed) {
     case 32: /////////// SPACEBAR //////////////////////////////
-      // if (!pouring) {
+      if (!pouring) {
         // create beer object and DOM element
       //  createBeer();
-        // pouring = true;
+      makeBeers();
+        pouring = true;
         // pouring the beer into the glass
         console.log("hello ",beers[beerCount]._beer);
         beers[beerCount]._beer.beer.css("display", "block");
         beers[beerCount]._beer.glass.animate({ height: "-=30" }, 700, function() {
           //// BEER IS FULL, ANIMATION COMPLETE
           // move the beer across the bar
-          beers[beerCount].beer.animate({ left: "-=460" }, 10000);
+          beers[beerCount]._beer.beer.animate({ left: "-=460" }, 10000);
           //beers[beerCount].movingToCustomer = true;
         //  pouringSent = true; // used in key up event
         });
-      // }
+      }
       // jump back to tap by pouring (space bar)
       $bartenderDiv.css("left", bartender._startX + "px");
       break;
@@ -81,7 +175,7 @@ $("body").on("keydown", function(evt) {
       if (bartender._newX < bar._padding) {
         bartender._newX = bar._padding;
       }
-      bartender._newX += "px";
+      // bartender._newX += "px";
       $bartenderDiv.css("left", bartender._newX);
       // TO DO : catch a beer glass
       break;
@@ -92,7 +186,7 @@ $("body").on("keydown", function(evt) {
       if (bartender._newX > bartender._startX) {
         bartender._newX = bartender._startX;
       }
-      bartender._newX += "px";
+      // bartender._newX += "px";
       $bartenderDiv.css("left", bartender._newX);
       break;
 
@@ -109,7 +203,7 @@ $("body").on("keydown", function(evt) {
           bartender._height / 2;
       }
       // // set the y position of bartender
-      bartender._newY += "px";
+      // bartender._newY += "px";
       $bartenderDiv.css("top", bartender._newY);
       // // set the x position of the bartender
       $bartenderDiv.css("left", bartender._startX);
@@ -128,7 +222,7 @@ $("body").on("keydown", function(evt) {
         bartender._newY = bartender._startY;
       }
       // set the y position of bartender
-      bartender._newY += "px";
+      // bartender._newY += "px";
       $bartenderDiv.css("top", bartender._newY);
       // set the x position of the bartender
       $bartenderDiv.css("left", bartender._startX);
@@ -139,27 +233,28 @@ $("body").on("keydown", function(evt) {
   }
 });
 /////////////////////////////////////////// KEY UP /////////////////
-// $("body").on("keyup", function(evt) {
-//   var keyPressed = event.which;
-//   switch (keyPressed) {
-//     case 32: //spacebar
-//       // check if the beer is pouring BUT not being sent to customer
-//       if (pouring && !pouringSent) {
-//         pouring = false;
-//         // stop pouring the beer into the glass
-//         beers[beerCount].beer.css("display", "none");
-//         beers[beerCount].beer.css("height", "30px");
-//         beers[beerCount].glass.stop();
-//       }
-//       // check if the beer is pouring AND being sent to customer
-//       if (pouring && pouringSent) {
-//         // now its ok to add another beer
-//          beerCount++;
-//          // reset these values for next beer
-//          pouring = false;
-//          pouringSent = false;
-//       }
-//     default:
-//       break;
-//   }
-// });
+$("body").on("keyup", function(evt) {
+  var keyPressed = event.which;
+  switch (keyPressed) {
+    case 32: //spacebar
+      // check if the beer is pouring BUT not being sent to customer
+      if (pouring && !pouringSent) {
+        pouring = false;
+        // stop pouring the beer into the glass
+        beers[beerCount]._beer.beer.css("display", "none");
+        beers[beerCount]._beer.beer.css("height", "30px");
+        beers[beerCount]._beer.glass.stop();
+      }
+      // check if the beer is pouring AND being sent to customer
+      if (pouring && pouringSent) {
+        beers.push(beer);
+        // now its ok to add another beer
+         beerCount++;
+         // reset these values for next beer
+         pouring = false;
+         pouringSent = false;
+      }
+    default:
+      break;
+  }
+});
