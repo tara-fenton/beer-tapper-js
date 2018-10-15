7f009c78ebc4852839c4e5b5dcadf5b1442ccc8f
Tara Fenton <tarafenton@ool-ad030165.dyn.optonline.net>
2018-10-15 19:05:49 -0400

    beers shake

--- public/js/BonusRound.js
+++ public/js/BonusRound.js
@@ -8,9 +8,24 @@ class BonusRound {
   setup() {
     this.bonusDiv.append(this.bonusText);
     $("#container").append(this.bonusDiv);
+    this.createBeers();
   }
 
+  createBeers() {
+    for (let i = 0; i < 6; i++) {
+      this.beersDiv = $("<div class='bonusBeers'></div>");
+      this.beersDiv.attr("id", "bonusBeer"+ i);
+      this.beersDiv.css("left", 110 + (i * 70)+"px");
+      this.bonusDiv.append(this.beersDiv);
+      this.shake(i);
+    }
+  }
 
+  shake(element) {
+    setTimeout(function() {
+      $("#bonusBeer" + element).effect("shake", { direction: "up", times: 4, distance: 10});
+    }, 900 * element);
+  }
 
 }
 export default BonusRound;
